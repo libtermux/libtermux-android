3654cd80c3b545cf347dedb7906b244c9d76b5f7
Nahid Hasan <githubnahid@gmail.com>
2026-04-01 15:56:08 +0600

    Update build.gradle.kts

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 24
+        minSdk = 26
         consumerProguardFiles("consumer-rules.pro")
 
         externalNativeBuild {
